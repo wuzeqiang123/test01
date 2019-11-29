99c3073323522fc7e141eb13c34ae32e4a8aea68
wuzeqiang123 <1599823709qq.com>
2019-11-29 21:24:29 +0800

    初始上传

---
+++ src/Test1.java
@@ -0,0 +1,10 @@
+/**
+ * @author wuzeqaing
+ * @description:
+ * @date 2019/11/29  21:15
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("进入git");
+    }
+}
